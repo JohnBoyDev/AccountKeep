82b60c2552276cce946098a2156daaa2f33da45c
John Channell <johnchannell97@gmail.com>
2020-01-27 17:22:37 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Account Keep</title>
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="logo.png">
+        <!-- Bootstrap CSS and JavaScript -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="main.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    </head>
+    <body class="bg-light">
+        <!-- Navigation Start -->
+        <div class="nav text-light bg-main m-0 py-1">
+            <div class="container inline-block">
+                <img src="logo.png" alt="Account Keep Logo" class="navbar-brand d-inline-block" style="width: auto;height: 12vh;">
+                <h3 class="d-inline-block align-middle">Account Keep</h3>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary my-0">
+            <div class="container">
+                <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    Navigation <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav text-center">
+                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">Features</a>
+                        <a class="nav-item nav-link" href="#">Pricing</a>
+                        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Start Main Content -->
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require("header.php"); ?>
+
+        <div class="jumbotron bg-dark text-light">
+            <div class="container">
+                <p class="h4">Account Keep</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, fugiat natus adipisci optio eum reiciendis, quisquam ut excepturi culpa corporis ad explicabo recusandae repellendus alias provident dolorem vero totam suscipit?</p>
+            </div>
+        </div>
+        <!-- Bootstrap Scripts -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+</html>
